56c5316b18e904bd3661d975870be82e1c7eabd2
Aanandalakshmi <128531206+Aanandalakshmi@users.noreply.github.com>
2025-09-01 21:01:40 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install necessary system dependencies for pyodbc
-sudo apt-get update
-sudo apt-get install -y unixodbc-dev g++
-
-# Start the application using a command like gunicorn or your specific startup command
-gunicorn --bind 0.0.0.0:8000 "your_app.wsgi:application"
